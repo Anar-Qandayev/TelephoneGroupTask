69a929ba0af5f34335e4b4cb764b890e3a3ac480
Anar-Qandayev <tu7bxfga8@code.edu.az>
2022-04-10 23:49:24 +0400

    commit

---
+++ CallTask/CallTask/Models/Contact.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CallTask.Models
+{
+    public class Contact
+    {
+        private static int _id;
+        public int Id { get; }
+        public string Fullname { get; set; }
+        public string Number { get; set; }
+
+        static Contact()
+        {
+            _id = 0;
+        }
+        private Contact()
+        {
+            _id++;
+            Id = _id;
+        }
+
+        public Contact(string fullname, string number) : this()
+        {
+            Fullname = fullname;
+            Number = number;
+        }
+    }
+}
